6bcd22d873c0082e2dddd678798bde2e08c3da55
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2020-12-29 00:36:25 +0700

    - modify jsosort using for loop

--- dist/generate-key.js
+++ dist/generate-key.js
@@ -1,8 +1,24 @@
 'use strict';
 
-const jsosort = require('jsosort');
-
 const sha1 = require('sha1');
+
+function jsosort(obj, sortfunction) {
+  var result = {};
+  var key = Object.keys(obj).sort(sortfunction);
+  var keylength = key.length;
+
+  for (var i = 0; i < keylength; i++) {
+    var value = obj[key[i]];
+
+    if (Object.prototype.toString.call(value) === '[object Object]') {
+      value = jsosort(value, sortfunction);
+    }
+
+    result[key[i]] = value;
+  }
+
+  return result;
+}
 
 module.exports = function init(obj) {
   obj = jsosort(obj);
